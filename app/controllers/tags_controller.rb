5599e8d3d6412c6cf6bf96255aefb27c81565e9a
Brit Butler <redline6561@gmail.com>
2015-10-28 11:30:22 -0400

    Fail early if tag cannot be found.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def tagged
-    @tag = Tag.find_by(name: params[:name])
+    @tag = Tag.find_by!(name: params[:name])
     @posts = @tag.posts
     render :tagged
   end
