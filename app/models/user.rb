568cd89e6b72f4cf990dfd69ab51d9f59166ac31
Brit Butler <redline6561@gmail.com>
2015-10-29 10:17:00 -0400

    Add basic user model.

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # validates :email, presence: true, uniqueness: true
+  # validates :password, presence: true
+
+  validates_length_of :password, minimum: 7
+  validates_uniqueness_of :email
+  validates_presence_of :email, :password
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028153416) do
+ActiveRecord::Schema.define(version: 20151029131137) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -41,5 +41,15 @@ ActiveRecord::Schema.define(version: 20151028153416) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "email",      null: false
+    t.string   "password",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
